27fa0faa9e0419cd363daeea50fbeb319d29c89c
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2022-06-28 15:40:45 -0300

    Criando a remoção com DELETE

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +26,7 @@ public class IndexController {
 	private UsuarioRepository usuarioRepository;
 	
 	@GetMapping("/{id}/codigo-venda/{venda}")
-	public ResponseEntity<Usuario> relatorio(@PathVariable Long id, @PathVariable Long venda) {
+	public ResponseEntity<Usuario> buscarCodigoDeVenda(@PathVariable Long id, @PathVariable Long venda) {
 	
 		Optional<Usuario> usuario = usuarioRepository.findById(id);
 		
@@ -51,6 +52,7 @@ public class IndexController {
 	
 	@PostMapping
 	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
+		
 		Usuario usuarioSalvo = usuarioRepository.save(usuario);
 		
 		return ResponseEntity.ok(usuarioSalvo);
@@ -74,4 +76,22 @@ public class IndexController {
 		
 		return ResponseEntity.ok(usuarioSalvo);
 	}
+	
+	@DeleteMapping("/{id}")
+	public String deletar(@PathVariable Long id) {
+		
+		usuarioRepository.deleteById(id);
+		
+		return "ok";
+	}
+	
+	@DeleteMapping("/{id}/venda")
+	public String deletarVenda(@PathVariable Long id) {
+		
+		/* Irá deletar todas as vendas do usuário */
+		usuarioRepository.deleteById(id);
+		
+		return "ok";
+	}
+	
 }
