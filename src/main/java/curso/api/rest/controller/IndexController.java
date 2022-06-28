1a0cf72e0ec69a84d278f603a8d97cc61f015c4d
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2022-06-28 13:03:09 -0300

    Criando serviço RESTful de consulta de usuário

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -1,41 +1,41 @@
 package curso.api.rest.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import curso.api.rest.model.Usuario;
+import curso.api.rest.repository.UsuarioRepository;
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping("/usuarios")
 public class IndexController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	/* Serviço RESTful */
-	@GetMapping("/")
-	public ResponseEntity<Usuario> init() {
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> init(@PathVariable Long id) {
 	
-		Usuario usuario = new Usuario();
-		usuario.setId(50L);
-		usuario.setLogin("leonardo_cordeirobahia@hotmail.com");
-		usuario.setNome("José Leonardo Cordeiro Bahia");
-		usuario.setSenha("15484844884");
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
 		
-		Usuario usuario2 = new Usuario();
-		usuario.setId(50L);
-		usuario.setLogin("asdad	@hotmail.com");
-		usuario.setNome("sdada");
-		usuario.setSenha("48484848");
+		return ResponseEntity.ok(usuario.get());
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> usuarios() {
 		
-		List<Usuario> usuarios = new ArrayList<>();
-		usuarios.add(usuario);
-		usuarios.add(usuario2);
+		List<Usuario> usuarios = usuarioRepository.findAll();
 		
-		return new ResponseEntity(usuarios, HttpStatus.OK);
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
 	}
 	
 }
