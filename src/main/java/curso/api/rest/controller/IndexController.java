eec1a105238a621fa15672da4343c1e1f8ab2654
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2022-06-28 13:39:19 -0300

    Criando nosso serviço POST e efetuando um cadastro de usuário

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +23,7 @@ public class IndexController {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
-	@GetMapping(value = "/{id}/codigo-venda/{venda}", produces = "application/pdf")
+	@GetMapping("/{id}/codigo-venda/{venda}")
 	public ResponseEntity<Usuario> relatorio(@PathVariable Long id, @PathVariable Long venda) {
 	
 		Optional<Usuario> usuario = usuarioRepository.findById(id);
@@ -31,7 +33,7 @@ public class IndexController {
 
 	/* Serviço RESTful */
 	@GetMapping("/{id}")
-	public ResponseEntity<Usuario> init(@PathVariable Long id) {
+	public ResponseEntity<Usuario> buscar(@PathVariable Long id) {
 	
 		Optional<Usuario> usuario = usuarioRepository.findById(id);
 		
@@ -39,11 +41,27 @@ public class IndexController {
 	}
 	
 	@GetMapping
-	public ResponseEntity<List<Usuario>> usuarios() {
+	public ResponseEntity<List<Usuario>> listar() {
 		
 		List<Usuario> usuarios = usuarioRepository.findAll();
 		
 		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return ResponseEntity.ok(usuarioSalvo);
+	}
+	
+	@PostMapping("/{idUser}/idVenda/{idVenda}")
+	public ResponseEntity<?> cadastrarVenda(@PathVariable Long idUser, @PathVariable Long idVenda) {
+		
+		/* Aqui seria o processo de venda */
+		//Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity("idUser: " + idUser + " idVenda: " + idVenda, HttpStatus.OK);
 	}
 	
 }
